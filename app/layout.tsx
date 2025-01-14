fd15dfb179610b01d1708835f94497576374ea25
Tahermaxse <hathitaher83@gmail.com>
2025-01-14 17:54:34 +0530

    SEO updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,55 @@ import type { Metadata } from 'next';
 import { Roboto_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 
-const inter = Roboto_Mono({ subsets: ['latin'] });
+const roboto_mono = Roboto_Mono({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Border Radius Calculator',
   description: 'A tool to calculate and visualize border radius values',
+  keywords: ['border radius', 'calculator', 'CSS tool', 'design', 'frontend'],
+  authors: [
+    { name: 'Taher Hathi', url: 'https://yourwebsite.com' },
+  ],
+  creator: 'Taher Hathi',
+  publisher: 'LoomUI',
+  openGraph: {
+    title: 'Border Radius Calculator',
+    description: 'A tool to calculate and visualize border radius values',
+    url: 'https://yourwebsite.com',
+    siteName: 'Border Radius Calculator',
+    images: [
+      {
+        url: 'https://yourwebsite.com/og-image.png',
+        width: 800,
+        height: 600,
+        alt: 'Border Radius Calculator',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@taher_max_',
+    creator: '@taher_max_',
+    title: 'Border Radius Calculator',
+    description: 'A tool to calculate and visualize border radius values',
+    images:'/og-image.png'
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+  },
+  viewport: 'width=device-width, initial-scale=1.0',
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: '#000000' },
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+  },
 };
 
 export default function RootLayout({
@@ -17,7 +61,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={roboto_mono.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
 import { Slider } from "@/components/ui/slider";
 import { Button } from "@/components/ui/button";
-import { ZoomIn, ZoomOut, Sun, Moon, Copy, Check } from "lucide-react";
+import { ZoomIn, ZoomOut, Sun, Moon, Copy, Check, Github } from "lucide-react";
 import { useTheme } from "next-themes";
 import GridPattern from "@/components/ui/grid-pattern";
 import { cn } from "@/lib/utils";
 import Footer from "@/components/Footer";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
 
 export default function Home() {
   const [padding, setPadding] = useState(40);
@@ -24,15 +31,13 @@ export default function Home() {
     setOuterRadius(calculatedOuterRadius);
   }, [innerRadius, padding]);
 
-  
-
   const handleInnerRadiusChange = (value: number[]) => {
-    const newInnerRadius = Math.min(value[0], 93); 
+    const newInnerRadius = Math.min(value[0], 93);
     setInnerRadius(newInnerRadius);
   };
-  
+
   const handlePaddingChange = (value: number[]) => {
-    const newPadding = Math.min(value[0], 41); 
+    const newPadding = Math.min(value[0], 41);
     setPadding(newPadding);
   };
 
@@ -66,26 +71,40 @@ export default function Home() {
 }`.trim();
 
   return (
-    <div className={`overflow-hidden min-h-screen p-4 md:p-6 ${theme === 'dark' ? 'bg-[#131313] text-white' : 'bg-gray-100 text-black'}`}>
+    <div
+      className={`overflow-hidden min-h-screen p-4 md:p-6 ${
+        theme === "dark" ? "bg-[#131313] text-white" : "bg-gray-100 text-black"
+      }`}
+    >
       <div className="max-w-8xl mx-auto grid grid-cols-1 lg:grid-cols-[1fr,300px] gap-4 md:gap-6">
         {/* Playground Area */}
-        <div className={`relative h-[585px] overflow-hidden backdrop-blur rounded-2xl p-4 md:p-6 ${theme === 'dark' ? 'bg-zinc-900/50' : 'bg-white/50'}`}>
-        <GridPattern
-        width={60}
-        height={60}
-        x={-1}
-        y={-1}
-        className={cn(
-          "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] ",
-        )}
-      />
+        <div
+          className={`relative h-[585px] overflow-hidden backdrop-blur rounded-2xl p-4 md:p-6 ${
+            theme === "dark" ? "bg-zinc-900/50" : "bg-white/50"
+          }`}
+        >
+          <GridPattern
+            width={60}
+            height={60}
+            x={-1}
+            y={-1}
+            className={cn(
+              "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
+            )}
+          />
           <div className="flex justify-start mb-6  md:mb-8">
             <div className="flex items-center gap-3 text-sm">
-              <span className="text-white bg-[#343537] px-2 py-1 rounded-md">R1</span>
+              <span className="text-white bg-[#343537] px-2 py-1 rounded-md">
+                R1
+              </span>
               <span className="dark:text-white">+</span>
-              <span className="text-white bg-[#343537] px-2 py-1 rounded-md">D</span>
+              <span className="text-white bg-[#343537] px-2 py-1 rounded-md">
+                D
+              </span>
               <span className="dark:text-white">=</span>
-              <span className="text-white bg-[#343537] px-2 py-1 rounded-md">R2</span>
+              <span className="text-white bg-[#343537] px-2 py-1 rounded-md">
+                R2
+              </span>
               <div className="flex gap-2 text-zinc-500 ml-2">
                 <span className="text-[#ec4899]">{innerRadius}</span>
                 <span>{padding}</span>
@@ -94,48 +113,62 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="relative aspect-square max-w-2xl max-h-[450px] mx-auto mt-[5rem]"
-               style={{ transform: `scale(${zoom})`, transformOrigin: "top left" }}>
+          <div
+            className="relative aspect-square max-w-2xl max-h-[450px] mx-auto mt-[5rem]"
+            style={{ transform: `scale(${zoom})`, transformOrigin: "top left" }}
+          >
             <div
               className="absolute inset-0 transition-all duration-200"
-              style={{ 
-                border: theme === 'dark' ? "2px solid rgba(255,255,255,0.1)" : "2px solid rgba(0,0,0,0.1)",
-                borderRadius: `${outerRadius}px` 
+              style={{
+                border:
+                  theme === "dark"
+                    ? "2px solid rgba(255,255,255,0.1)"
+                    : "2px solid rgba(0,0,0,0.1)",
+                borderRadius: `${outerRadius}px`,
               }}
             >
-              <div 
+              <div
                 className="absolute top-0 left-0 w-[120px] h-[120px]"
-                style={{ 
+                style={{
                   borderTop: "4px solid #6366f1",
                   borderLeft: "4px solid #6366f1",
                   borderTopLeftRadius: `${outerRadius}px`,
-                  opacity: 0.8
-                }} 
+                  opacity: 0.8,
+                }}
               />
             </div>
             <div
               className="absolute transition-all duration-200 "
               style={{
                 inset: `${padding}px`,
-                border: theme === 'dark' ? "2px solid rgba(255,255,255,0.1)" : "2px solid rgba(0,0,0,0.1)",
+                border:
+                  theme === "dark"
+                    ? "2px solid rgba(255,255,255,0.1)"
+                    : "2px solid rgba(0,0,0,0.1)",
                 borderRadius: `${innerRadius}px`,
               }}
             >
-              <div 
+              <div
                 className="absolute top-0 right-0 w-[120px] h-[120px]"
-                style={{ 
+                style={{
                   borderTop: "4px solid #ec4899",
                   borderRight: "4px solid #ec4899",
                   borderTopRightRadius: `${innerRadius}px`,
-                  opacity: 0.8
-                }} 
+                  opacity: 0.8,
+                }}
               />
             </div>
             {zoom >= 1.5 && (
               <>
-                <span className="absolute top-0 left-1/5 text-sm text-[#6366f1]">{outerRadius}px</span>
-                <span className="absolute top-[45px] left-1/4 text-sm text-[#ec4899]">{innerRadius}px</span>
-                <span className="absolute top-[20px] left-[65px] text-sm text-zinc-400">{padding}px</span>
+                <span className="absolute top-0 left-1/5 text-sm text-[#6366f1]">
+                  {outerRadius}px
+                </span>
+                <span className="absolute top-[45px] left-1/4 text-sm text-[#ec4899]">
+                  {innerRadius}px
+                </span>
+                <span className="absolute top-[20px] left-[65px] text-sm text-zinc-400">
+                  {padding}px
+                </span>
               </>
             )}
           </div>
@@ -172,13 +205,35 @@ export default function Home() {
                 <Moon className="h-4 w-4" />
               )}
             </Button>
+            <TooltipProvider delayDuration={0}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link href="https://github.com/Tahermaxse/borderradiuscalc.git">
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="h-8 w-8 dark:hover:bg-zinc-800"
+                  >
+                    <Github className="h-4 w-4" />
+                  </Button>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent className="px-2 py-1 text-xs">
+                ⭐ Star us on GitHub
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
         </div>
 
         {/* Controls Sidebar */}
-        <div className={`backdrop-blur rounded-2xl p-4 md:p-6 space-y-6 md:space-y-8 ${theme === 'dark' ? 'bg-[#1f1f21]' : 'bg-white/50'}`}>
+        <div
+          className={`backdrop-blur rounded-2xl p-4 md:p-6 space-y-6 md:space-y-8 ${
+            theme === "dark" ? "bg-[#1f1f21]" : "bg-white/50"
+          }`}
+        >
           <h2 className="text-lg md:text-xl font-semibold mb-6">Configure</h2>
-          
+
           <div className="space-y-6">
             <div className="space-y-4">
               <div className="flex justify-between items-center">
@@ -215,8 +270,12 @@ export default function Home() {
                 <label className="text-sm font-medium">Outer radius</label>
                 <span className="text-sm text-zinc-400">{outerRadius}px</span>
               </div>
-              <div className={`w-full h-2 rounded-full ${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}`}>
-                <div 
+              <div
+                className={`w-full h-2 rounded-full ${
+                  theme === "dark" ? "bg-zinc-800" : "bg-gray-200"
+                }`}
+              >
+                <div
                   className="h-full bg-blue-500 rounded-full transition-all duration-200"
                   style={{ width: `${(outerRadius / 200) * 100}%` }}
                 />
@@ -228,7 +287,9 @@ export default function Home() {
                 <label className="text-sm font-medium">min-radius</label>
                 <span className="text-sm text-zinc-400">{minRadius}px</span>
               </div>
-              <p className="text-xs text-zinc-500">Prevents radius from going below</p>
+              <p className="text-xs text-zinc-500">
+                Prevents radius from going below
+              </p>
             </div>
           </div>
 
@@ -248,17 +309,25 @@ export default function Home() {
                 )}
               </Button>
             </div>
-            <pre className={`p-4 rounded-bl-lg rounded-br-lg text-xs ${theme === 'dark' ? 'bg-[#292a2c]' : 'bg-gray-100'}`}>
+            <pre
+              className={`p-4 rounded-bl-lg rounded-br-lg text-xs ${
+                theme === "dark" ? "bg-[#292a2c]" : "bg-gray-100"
+              }`}
+            >
               <code className="text-[#6366f1]">.outer-box</code> {"{"}
               <br />
-              <code className="text-zinc-400 pl-4">border-radius: {outerRadius}px;</code>
+              <code className="text-zinc-400 pl-4">
+                border-radius: {outerRadius}px;
+              </code>
               <br />
               {"}"}
               <br />
               <br />
               <code className="text-[#ec4899]">.inner-box</code> {"{"}
               <br />
-              <code className="text-zinc-400 pl-4">border-radius: {innerRadius}px;</code>
+              <code className="text-zinc-400 pl-4">
+                border-radius: {innerRadius}px;
+              </code>
               <br />
               {"}"}
             </pre>
@@ -266,7 +335,7 @@ export default function Home() {
         </div>
       </div>
       <div className="mt-4">
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
